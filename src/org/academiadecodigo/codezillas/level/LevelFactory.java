b602a59f39da9853691bf84a43ecedbf1484e20f
Tiago Silva <Tiago.MB.Silva@edu.azores.gov.pt>
2019-06-20 21:57:02 +0000

    baseGame branch

---
+++ src/org/academiadecodigo/codezillas/level/LevelFactory.java
@@ -0,0 +1,13 @@
+package org.academiadecodigo.codezillas.level;
+
+import org.academiadecodigo.codezillas.gameObjects.Platform;
+
+public class LevelFactory {
+
+    Platform[] platforms = new Platform[100];
+
+    public void createObjects(){
+
+    }
+
+}
