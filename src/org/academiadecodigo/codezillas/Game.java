0e5d1038cd69e24675acfb7c0fb3639438d6d91d
João Tavares <jc.992@hotmail.com>
2019-06-21 20:45:44 +0000

    deleted messy stuff

--- src/org/academiadecodigo/codezillas/Game.java
+++ src/org/academiadecodigo/codezillas/Game.java
@@ -42,7 +42,6 @@ public class Game {
             }
 
             while (true) {
-                projectiles[0].setMoving();
                 int rng = (int) (Math.random() * projectiles.length + 1);
 
                 for (int i = 0; i < projectiles.length-1; i++) {
@@ -65,40 +64,7 @@ public class Game {
                     e.printStackTrace();
                 }
 
-
-                ////PROJECTILE TESTS
-
-            /*projectiles = new Projectile[15];
-            int positionPlacer = 0;
-
-            for (int i = 0; i < projectiles.length; i++) {
-
-                projectiles[i] = new Projectile(i + positionPlacer, 0);
-                projectiles[i].draw();
-                positionPlacer += 1;
             }
-
-            while (true) {
-
-                projectiles[0].fallingDown();
-
-                try {
-                    Thread.sleep(60);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-
-                if (projectiles[0].getPos().getRow() == 800) {
-                    projectiles[0].delete();
-                    projectiles[0].setPos(0, 0);
-                    projectiles[0].resetFireball();
-                    projectiles[0].draw();*/
-                // }
-
-                //}
-            }
-
-
         }
     }
 }
